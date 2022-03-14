263d32505d864de45115b4f5af29bb28a16b0664
Riday <rafi.riday@gmail.com>
2022-03-14 13:23:11 +0600

    Add files via upload

---
+++ sample/ccpc.11.b/js/update_teacher_info.js
@@ -0,0 +1,9 @@
+/* Permanent Starts */
+
+SKS = { link:"https://us05web.zoom.us/j/2346276403?pwd=R3E2SEdsRlJYaU1Cdi8zZ0pzekREZz09", name:"Bio - SKS", pmi:"2346276403", password:"ccpc" };
+
+let KMK = { link:"https://us04web.zoom.us/j/3315664512?pwd=MmV5ZHpZSUpjd0ZaTTVUT1B5a2VrQT09", name:"Phy - KMK", pmi:"3315664512", password:"ccpc" };
+
+/* Permanent Finished */
+
+/* Temporary Update */
