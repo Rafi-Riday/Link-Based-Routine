dc917702e61d406d3701836e79433b9777f56088
Riday <rafi.riday@gmail.com>
2022-03-14 13:14:31 +0600

    Add files via upload

---
+++ ccpc.11.b/js/teacher_info.js
@@ -0,0 +1,35 @@
+let RI = { link:"https://us04web.zoom.us/j/7749229475?pwd=eDFlUXg0U2pXR2dSdmtRZzg5ajY3QT09", name:"Phy - RI", pmi:"7749229475", password:"photon" };
+
+let SA = { link:"https://us04web.zoom.us/j/6154854785?pwd=QkFFWGNKQnBncGNZWWIvVVpnOE1jZz09", name:"ICT - SA", pmi:"6154854785", password:"sa10SA20" };
+
+let NF = { link:"https://us04web.zoom.us/j/4624791711?pwd=3fXM8xnV-Hw", name:"Bio - NF", pmi:"4624791711", password:"ccpc" };
+
+let BA = { link:"https://us04web.zoom.us/j/6481271305?pwd=a3J3TXl1UU01UzhKM0RMV0RMWGVmUT09", name:"ICT - BA", pmi:"6481271305", password:"ccpc" };
+
+let SYC = { link:"https://us04web.zoom.us/j/4552359732?pwd=TFV5dHpDTVhyTHA4aXNPaEdtNW9UQT09", name:"Bang - SYC", pmi:"4552359732", password:"ccpc" };
+
+let MDA = { link:"https://us04web.zoom.us/j/2807011163?pwd=TmVDZWZSRk1oY0I0VDhQY2xld3VFZz09", name:"Chem - MDA", pmi:"2807011163", password:"ccpc" };
+
+let HB = { link:"https://us04web.zoom.us/j/4357773857?pwd=M2FSUHlhcGJ4THZsYzJCQVA5YjBGdz09", name:"Eng - HB", pmi:"4357773857", password:"6791" };
+
+let RBA = { link:"https://us04web.zoom.us/j/5258498691?pwd=QWhwRjdSTWNVaUxPLzRHcGFLOFQxdz09", name:"Phy - RBA", pmi:"5258498691", password:"zoom" };
+
+let SK = { link:"https://us04web.zoom.us/j/3061810144?pwd=SlZpS0FWY3VGUW9jMHNDc2xmUUF1UT09", name:"Bio - SK", pmi:"3061810144", password:"ccpc" };
+
+let PB = { link:"https://us04web.zoom.us/j/3709607756?pwd=MmdrTVBod21YbE1SeWx2VnYrSUkyZz09", name:"Eng - PB", pmi:"3709607756", password:"P11p02" };
+
+let SCN = { link:"https://us04web.zoom.us/j/9416347426?pwd=dytrT1c2Mlh5V0JXQ2FkVVdnVDBFUT09", name:"Math - SCN", pmi:"9416347426", password:"ccpc" };
+
+let SPM = { link:"https://us04web.zoom.us/j/3140359368?pwd=rCE5rFNX2CfnmTG2GCJEKcO3ljgw7F.1", name:"Chem - SPM", pmi:"3140359368", password:"ccpc" };
+
+let MAH = { link:"https://us04web.zoom.us/j/8908769561?pwd=3fXM8xnV-Hw", name:"Bang - MAH", pmi:"8908769561", password:"ccpc" };
+
+let MMA = { link:"https://us04web.zoom.us/j/7225249489?pwd=dDFuSEJVb0VNaXRTUW9TWjhqT2RrZz09", name:"Math - MMA", pmi:"7225249489", password:"conic" };
+
+let KS = { link:"https://us04web.zoom.us/j/8828046590?pwd=Qlhtb3FUYWVTK0pvcE03YURXOWF1UT09", name:"ICT - KS", pmi:"8828046590", password:"CcPC" };
+
+let SKS = { link:"https://us05web.zoom.us/j/2346276403?pwd=R3E2SEdsRlJYaU1Cdi8zZ0pzekREZz09", name:"Eng - SKS", pmi:"2346276403", password:"ccpc" };
+
+let practical_1 = { link:"", name:"", pmi:"", password:"" };
+
+let practical_2 = { link:"", name:"", pmi:"", password:"" };
